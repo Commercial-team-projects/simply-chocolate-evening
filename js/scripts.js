f7d24137cd522326a0b11da928d99caab17d16d1
ValeriiaM <moskalenkovvy@gmail.com>
2025-09-16 19:14:47 +0300

    add menu close after inner navigation. add menu and styles

--- js/scripts.js
+++ js/scripts.js
@@ -2,9 +2,10 @@ const modalLayer = document.querySelector(".modal-overlay");
 const modalOpenBtn = document.querySelector(".loved-button");
 const modalCloseBtn = document.querySelector(".modal-close-btn");
 
+const mMenuLayer = document.querySelector(".mobile-menu");
 const mMenuBurgerBtn = document.querySelector(".burger-btn");
 const mMenuCloseBtn = document.querySelector(".close-btn");
-const mMenuLayer = document.querySelector(".mobile-menu");
+const mMenuInnerLinks = document.querySelector(".mobile-menu .navigation-list");
 
 modalOpenBtn.addEventListener("click", (event) => {
   {
@@ -23,5 +24,12 @@ mMenuBurgerBtn.addEventListener("click", (event) => {
 });
 
 mMenuCloseBtn.addEventListener("click", (event) => {
+  mMenuLayer.classList.remove("is-open");
+});
+
+mMenuInnerLinks.addEventListener("click", (event) => {
+  if (event.target.nodeName !== "A") {
+    return;
+  }
   mMenuLayer.classList.remove("is-open");
 });
