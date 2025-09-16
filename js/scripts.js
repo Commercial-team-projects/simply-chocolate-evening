141621e660f29d09be28f650b58a8f0f6115d109
ValeriiaM <moskalenkovvy@gmail.com>
2025-09-16 18:57:00 +0300

    move script to script file. fix button styles

--- js/scripts.js
+++ js/scripts.js
@@ -2,6 +2,10 @@ const modalLayer = document.querySelector(".modal-overlay");
 const modalOpenBtn = document.querySelector(".loved-button");
 const modalCloseBtn = document.querySelector(".modal-close-btn");
 
+const mMenuBurgerBtn = document.querySelector(".burger-btn");
+const mMenuCloseBtn = document.querySelector(".close-btn");
+const mMenuLayer = document.querySelector(".mobile-menu");
+
 modalOpenBtn.addEventListener("click", (event) => {
   {
     modalLayer.classList.add("is-open");
@@ -12,4 +16,12 @@ modalCloseBtn.addEventListener("click", (event) => {
   {
     modalLayer.classList.remove("is-open");
   }
+});
+
+mMenuBurgerBtn.addEventListener("click", (event) => {
+  mMenuLayer.classList.add("is-open");
+});
+
+mMenuCloseBtn.addEventListener("click", (event) => {
+  mMenuLayer.classList.remove("is-open");
 });
